716f6fc452af980e7de962aebd98cc68c9749643
Rajan Kandel <kandelrajan141@gmail.com>
2026-07-23 18:18:15 +0545

    added a play with friend option and a score_board between X and O also added a reset score button that resets score every time

--- script.js
+++ script.js
@@ -5,17 +5,58 @@ let start = document.querySelector(".start")
 let chosenSymbol = "X";
 let notChosenSymbol = "O";
 let nums = [];
+let gameMode = "frn"
+let p1 = "X"
+let p2 = "O"
+let p
 
 
+let x_win = Number(localStorage.getItem("x_win")) || 0;
+let o_win = Number(localStorage.getItem("o_win")) || 0;
 
-// console.log(box1.innerHTML)
 
+document.querySelector(".x_win_count").innerHTML = x_win;
+document.querySelector(".o_win_count").innerHTML = o_win;
+
+
+function reset_score() {
+    localStorage.setItem("x_win", 0)
+    localStorage.setItem("o_win", 0)
+    location.reload()
+}
 
 function Choose(symbl) {
     chosenSymbol = symbl
     start.innerHTML = `Start as ${symbl}`;
     start.style = "display:flex";
     choose.style = "display:none";
+}
+
+function frn() {
+    document.querySelector(".play_with_frn").style = "display:none"
+    document.querySelector(".play_with_comp").style = "display:flex"
+    document.querySelector(".first_x").style = "display:flex"
+    document.querySelector(".choose").style = "display:none"
+    document.querySelector(".wins").style = "display:flex"
+
+    for(let i=1;i<=9;i++){
+        document.querySelector(`.box${i}`).innerHTML = ""
+    }
+
+    gameMode = "frn"
+}
+function comp() {
+    document.querySelector(".play_with_frn").style = "display:flex"
+    document.querySelector(".play_with_comp").style = "display:none"
+    document.querySelector(".first_x").style = "display:none"
+    document.querySelector(".choose").style = "display:flex"
+    document.querySelector(".wins").style = "display:none"
+
+    for(let i=1;i<=9;i++){
+        document.querySelector(`.box${i}`).innerHTML = ""
+    }
+    
+    gameMode = "comp"
 }
 
 function clicked(num) {
@@ -25,23 +66,30 @@ function clicked(num) {
     if (box.innerHTML !== "") {
         return;
     }
-    if (start.innerHTML == "Start as X" || start.innerHTML == "Start as O") {
+    if (gameMode === "frn") {
+
         let box = document.querySelector(`.box${num}`)
-        box.innerHTML = `${chosenSymbol}`
-        if (chosenSymbol === "O") {
-            notChosenSymbol = "X"
+        box.innerHTML = `${p1}`
+
+        p = p2
+        p2 = p1
+        p1 = p
+    }
+    else if (gameMode === "comp") {
+        if (start.innerHTML == "Start as X" || start.innerHTML == "Start as O") {
+            let box = document.querySelector(`.box${num}`)
+            box.innerHTML = `${chosenSymbol}`
+            if (chosenSymbol === "O") {
+                notChosenSymbol = "X"
+            }
+            let random_box;
+
+            do {
+                random_box = Math.floor(Math.random() * 9) + 1;
+            } while (document.querySelector(`.box${random_box}`).innerHTML !== "");
+
+            document.querySelector(`.box${random_box}`).innerHTML = notChosenSymbol;
         }
-        // let random_box = Math.floor(Math.random() * 9) + 1
-
-        // nums.push(num);
-        // nums.push(random_box);
-        let random_box;
-
-        do {
-            random_box = Math.floor(Math.random() * 9) + 1;
-        } while (document.querySelector(`.box${random_box}`).innerHTML !== "");
-
-        document.querySelector(`.box${random_box}`).innerHTML = notChosenSymbol;
     }
     let box1 = document.querySelector(".box1").innerHTML
     let box2 = document.querySelector(".box2").innerHTML
@@ -52,31 +100,60 @@ function clicked(num) {
     let box7 = document.querySelector(".box7").innerHTML
     let box8 = document.querySelector(".box8").innerHTML
     let box9 = document.querySelector(".box9").innerHTML
-    if (box1 === box2 && box2 === box3 && box1 !== "") {
-        alert(`${box1} WON`);
-    }
-    else if (box1 === box4 && box4 === box7 && box1 !== "") {
-        alert(`${box1} WON`);
-    }
-    else if (box1 === box5 && box5 === box9 && box1 !== "") {
-        alert(`${box1} WON`);
+
+
+    if ((box1 === box2 && box2 === box3 && box1 !== "") || (box1 === box4 && box4 === box7 && box1 !== "") || (box1 === box5 && box5 === box9 && box1 !== "")) {
+        alert(`${box1.toUpperCase()} WON`);
+        if(gameMode==="frn"){
+            if (box1 === "X") {
+                x_win += 1
+                localStorage.setItem("x_win", x_win)
+            }
+            if (box1 === "O") {
+                o_win += 1
+                localStorage.setItem("o_win", o_win)
+            }
+        }
     }
     else if (box3 === box6 && box6 === box9 && box3 !== "") {
-        alert(`${box6} WON`);
+        alert(`${box6.toUpperCase()} WON`);
+        if(gameMode==="frn"){
+            if (box6 === "X") {
+                x_win += 1
+                localStorage.setItem("x_win", x_win)
+            }
+            if (box6 === "O") {
+                o_win += 1
+                localStorage.setItem("o_win", o_win)
+            }
+        }
     }
     else if (box7 === box8 && box8 === box9 && box7 !== "") {
-        alert(`${box8} WON`);
+        alert(`${box8.toUpperCase()} WON`);
+        if(gameMode==="frn"){
+            if (box8 === "X") {
+                x_win += 1
+                localStorage.setItem("x_win", x_win)
+            }
+            if (box8 === "O") {
+                o_win += 1
+                localStorage.setItem("o_win", o_win)
+            }
+        }
     }
-    else if (box4 === box5 && box5 === box6 && box4 !== "") {
-        alert(`${box5} WON`);
+    else if ((box4 === box5 && box5 === box6 && box4 !== "") || (box2 === box5 && box5 === box8 && box2 !== "") || (box3 === box5 && box5 === box7 && box3 !== "")) {
+        alert(`${box5.toUpperCase()} WON`);
+        if(gameMode==="frn"){
+            if (box5 === "X") {
+                x_win += 1
+                localStorage.setItem("x_win", x_win)
+            }
+            if (box5 === "O") {
+                o_win += 1
+                localStorage.setItem("o_win", o_win)
+            }
+        }
     }
-    else if (box2 === box5 && box5 === box8 && box2 !== "") {
-        alert(`${box5} WON`);
-    }
-    else if (box3 === box5 && box5 === box7 && box3 !== "") {
-        alert(`${box5} WON`);
-    }
-
     // console.log(nums);
 }
 
